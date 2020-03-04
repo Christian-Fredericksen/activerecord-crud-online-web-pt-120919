b9c861dcb9c86edf38320cd43c65622c2493d3d3
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-04 18:40:59 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_movies.rb
+++ db/migrate/01_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.integer :size 
-      t.text :image_url
-      t.timestamps
+      t.string :title
+      t.integer :release_date 
+      t.string :director 
+      t.string :lead 
+      t.boolean :in_theaters 
     end
   end
 end
