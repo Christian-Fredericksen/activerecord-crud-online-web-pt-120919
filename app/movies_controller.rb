fe75a99453745cda848465e17c62ba33d7e05e99
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-04 21:25:11 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,6 +63,8 @@ def can_find_using_where_clause_and_be_sorted
   # release date descending
   Movie.all WHERE :release_date > 2002
   Movie.order_by :release_date.descending 
+  
+  
 end
 
 def can_be_found_updated_and_saved
