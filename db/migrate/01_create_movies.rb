d7254289e05b645bcbb9f87e49605c6d4a54e989
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-04 18:37:45 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_movies.rb
+++ db/migrate/01_create_movies.rb
@@ -1,5 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size 
+      t.text :image_url
+      t.timestamps
+    end
   end
 end
